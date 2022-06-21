c173fc6b98dd836866e00c5577d8f7a489ebac66
Fran Bec <81979771+franBec@users.noreply.github.com>
2022-06-20 22:27:28 -0300

    [v.1.6 oops] forgot something in test.jsx that made the compilation in vercel crash

--- pages/test.jsx
+++ pages/test.jsx
@@ -1,18 +1,5 @@
 import Layout from '../components/_utils/layout'
-import { useState } from 'react'
-
-//https://stackoverflow.com/questions/60793727/failed-to-load-pdf-file-in-react-js
-import { Document, Page, pdfjs } from 'react-pdf'
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
-
 const Test = () => {
-  const [numPages, setNumPages] = useState(null)
-  const [pageNumber, setPageNumber] = useState(1)
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages)
-  }
-
   return (
     <Layout
       headTittle="Pollito's stuff | Testing area"
@@ -24,14 +11,7 @@ const Test = () => {
       introSignature="Pollito, while copying StackOverflow codes to check if it is what he's looking for"
       isThisHome={false}
     >
-      <>
-        <Document file="/pdf/sample.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-          <Page pageNumber={pageNumber} />
-        </Document>
-        <p>
-          Page {pageNumber} of {numPages}
-        </p>
-      </>
+      <>Jai!</>
     </Layout>
   )
 }
